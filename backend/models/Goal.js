a67f7881fd794abcec46121e7ec43e11339d478b
Bhavesh Chauhan <130345635+cbhavesh09@users.noreply.github.com>
2025-11-21 08:33:01 +0530

    Refactor Goal model schema for clarity

--- backend/models/Goal.js
+++ backend/models/Goal.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 const goalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Assuming userId should always be present
-  day: { type: Number, required: true },     // Make day required
-  distance: { type: Number, required: true }, // Make distance required
-  totalgoals: { type: Number, required: false }, // Optional field
-  progress: { type: Number, required: false },   // Optional field
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+  day: { type: Number, required: true },   
+  distance: { type: Number, required: true }, 
+  totalgoals: { type: Number, required: false }, 
+  progress: { type: Number, required: false },  
   createdAt: { type: Date, default: Date.now }
 });
 

0b4fc1632517d34d35aed86476ce040de275729b
Bhavesh Chauhan <130345635+cbhavesh09@users.noreply.github.com>
2025-11-21 08:34:40 +0530

    Update metrics.js

--- backend/routes/metrics.js
+++ backend/routes/metrics.js
@@ -9,18 +9,18 @@ router.get('/calculate/:userId', async (req, res) => {
   const { userId } = req.params;
 
   try {
-    // Fetch all goals and calculate total distance covered
+ 
     const goals = await Goal.find({ userId });
     const totalDistance = goals.reduce((sum, goal) => sum + goal.distance, 0);
 
-    // Fetch user data to determine calories per mile
+
     const user = await User.findById(userId);
-    const weight = user?.weight || 155; // Default to 155 lbs if weight not found
+    const weight = user?.weight || 155; 
 
     const caloriesPerKm = weight <= 125 ? 80 : weight <= 155 ? 100 : 120;
     const caloriesBurned = (totalDistance * caloriesPerKm)/100;
 
-    // Calculate weight lost by comparing user's current weight with latest workout weight
+
     const latestWorkout = await Workout.findOne({ userId }).sort({ date: -1 });
     const latestWorkoutWeight = latestWorkout?.weight || weight;
     const weightLost = weight - latestWorkoutWeight;
